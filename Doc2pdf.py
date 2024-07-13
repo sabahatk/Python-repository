8a2591a11cede0f14d0efffa5065a6429673e9d5
Mohammad Sabahat Khan <sabahatkhan@live.ca>
2024-07-13 10:32:51 -0400

    Created doc2pdf program

---
+++ Doc2pdf.py
@@ -0,0 +1,3 @@
+from docx2pdf import convert
+
+convert(r"C:\Users\sabah\Documents\Sabahat\Job\Resumes and Cover Letters to Upload\Sabahat Khan Resume 2.docx", r"C:\Users\sabah\Documents\Sabahat\Job\Resumes and Cover Letters to Upload\Sabahat Khan Resume.pdf")
